9210ed75c6c7ed6be847a132c2a39dda8a9d8a10
Kyle McDonald <kyle@kylemcdonald.net>
2016-04-19 18:18:02 -0400

    remove solo clips and collapse empty time

--- server.js
+++ server.js
@@ -36,30 +36,48 @@ app.get('/get_clips', function(req, res) {
   });
 });
 
+app.get('/get_sorted', function(req, res) {
+  clips.find({}).toArray(function(err, data) {
+    data.sort(function (a, b) {
+      return a.startoff < b.startoff ? -1 : +1;
+    })
+    res.json(data);
+  });
+});
+
 http.listen(3000, function(){
   console.log('listening on port 3000');
 });
 
-
 function resetDb() {
   database.dropCollection('clips', function(err) {
     var folders = fs.readdirSync(footage_root);
-    folders.forEach(function(folder) {
-      console.log(folder);
-      var path = footage_root + folder;
-      if (fs.lstatSync(path).isDirectory()) {
-        var files = fs.readdirSync(path);
-        files.forEach(function(f) {
-          if (f.indexOf('.mp4') !== -1 || f.indexOf('.mov') !== -1) {
-            addClip(folder, f);
-          }
-        });
-      }
-    });
+    var data = buildClips(folders);
+    data = removeSoloClips(data);
+    data = collapseEmptySpace(data);
+    console.log(data.length);
+    data.map(addClip);
   });
 }
 
-function addClip(folder, file) {
+function buildClips(folders) {
+  var data = [];
+  folders.forEach(function(folder) {
+    console.log(folder);
+    var path = footage_root + folder;
+    if (fs.lstatSync(path).isDirectory()) {
+      var files = fs.readdirSync(path);
+      files.forEach(function(f) {
+        if (f.indexOf('.mp4') !== -1 || f.indexOf('.mov') !== -1) {
+          data.push(buildClip(folder, f));
+        }
+      });
+    }
+  });
+  return data;
+}
+
+function buildClip(folder, file) {
   var toks = folder.split('_');
   var type = toks[0];
   var person = toks[1];
@@ -67,9 +85,10 @@ function addClip(folder, file) {
   var time = file.substring(day.length+1, file.indexOf('_+')).replace(/_/g, ':');
   var startoff = new Date(day+' '+time).getTime() - beginOff;
   var duration = parseInt(file.substring(file.indexOf('_+')+2, file.length-4))*1000;
+  duration = Math.max(1000, duration - 1000); // cut off the last second if possible
   var endoff = startoff + duration;
-  console.log(startoff, duration, endoff);
-  var c = {
+  // console.log(startoff, duration, endoff);
+  return {
     path: '/footage/'+folder+'/'+file,
     type: type,
     person: person,
@@ -77,7 +96,47 @@ function addClip(folder, file) {
     endoff: endoff,
     duration: duration
   };
-  clips.insertOne(c, function(err, res) {
-    console.log('successfully entered '+folder+':'+file);
-  });
+}
+
+function removeSoloClips(data) {
+  return data.filter(function (a) {
+    return data.some(function (b) {
+      return a.startoff == b.startoff && a.path != b.path;
+    });
+  })
+}
+
+function collapseEmptySpace(data, inbetweenTime) {
+  inbetweenTime = inbetweenTime || 10 * 1000; // 10 seconds
+  var previousEndoff = 0;
+  var timeOffset = 0;
+  data.sort(function (a, b) {
+      return a.startoff < b.startoff ? -1 : +1;
+  })
+  data.forEach(function (item) {
+    // if the current start time
+    // happens after the previousEndoff
+    if (item.startoff > previousEndoff) {
+      // then we need to collapse that time
+      timeOffset -= (item.startoff - previousEndoff);
+      // if this isn't the first item
+      if(previousEndoff > 0) {
+        // add a little buffer
+        timeOffset += inbetweenTime;
+      }
+    }
+    // keep track of the most recent endoff
+    // (do this before modifying the endoff)
+    previousEndoff = item.endoff;
+    // and apply the offset now and for all future items
+    item.startoff += timeOffset;
+    item.endoff += timeOffset;
+  })
+  return data;
+}
+
+function addClip(clip) {
+  clips.insertOne(clip, function(err, res) {
+    console.log('successfully entered ' + clip.path);
+  });  
 }
